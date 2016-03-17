5ffbce78697eac7701b717214c631d202e0afd9c
Isaque Rocha <isaquerocha@gmail.com>
2016-03-17 19:11:27 -0300

    Adicionado Faker ao seeder de banco de dados.

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\WorkSchedule;
 
 class UserTableSeeder extends Seeder
 {
@@ -37,5 +39,38 @@ class UserTableSeeder extends Seeder
            'user_id' => \App\User::orderBy('id', 'desc')->first()->id,
        ]);
 
+       $faker = \Faker\Factory::create('pt_BR');
+
+        // User::truncate();
+
+
+       for ($i=0; $i < 10; $i++) {
+           DB::table('users')->insert([
+               'name'           =>  $faker->name,
+               'email'          =>  $faker->email,
+               'cpf'            =>  $faker->cpf,
+               'phone'          =>  $faker->cellphoneNumber,
+               'address'        =>  $faker->address,
+               'role'           =>  $faker->randomElement(["Coordenador", "Técnico", "Estagiário"]),
+               'area'           =>  $faker->randomElement(["Diagramação", "Gerência", "Ilustração", "Vídeo", "Web"]),
+               'entrance_date'  =>  \Carbon\Carbon::now(),
+               'password'       =>  bcrypt('password'),
+           ]);
+
+           $user = User::orderBy('id', 'desc')->first();
+
+           $user->work_schedule()->create([
+               'seg_am' => '08:00 às 12:00',
+               'seg_pm' => '13:00 às 17:00',
+               'ter_am' => '08:00 às 12:00',
+               'ter_pm' => '13:00 às 17:00',
+               'qua_am' => '08:00 às 12:00',
+               'qua_pm' => '13:00 às 17:00',
+               'qui_am' => '08:00 às 12:00',
+               'qui_pm' => '13:00 às 17:00',
+               'sex_am' => '08:00 às 12:00',
+               'sex_pm' => '13:00 às 17:00',
+           ]);
+       }
     }
 }
