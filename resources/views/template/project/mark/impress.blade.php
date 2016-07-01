424e4eccd21430f8d7919babb98df7e15d40337f
Isaque Rocha <isaquerocha@gmail.com>
2016-07-01 16:28:19 -0300

    Modificações de teste.

--- resources/views/template/project/mark/impress.blade.php
+++ resources/views/template/project/mark/impress.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-3">
+<div class="col-md-4">
     <div class="box box-ldi">
         <div class="panel-group">
             <div class="panel panel-default">
@@ -372,13 +372,12 @@
                                 </div>
                             </div>
                         </form>
-                        <hr>
+                        <hr class="row">
                         <div style="text-align:right;">
                             <button type="button" class="btn btn-default pull-left" data-toggle="modal" data-target="#editLivroImpresso" title="Editar atividade">
-                                Editar atividade
+                                Editar
                             </button>
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                            <button type="button" class="btn btn-success">Salvar</button>
+                            <button type="button" class="btn btn-success" title="Salvar">Salvar</button>
                         </div>
                     </div>
                 </div>
